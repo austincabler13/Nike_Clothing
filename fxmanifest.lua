9a3c65fb282150f09591ca3aa5a0aca29f2ae29b
austincabler13 <austincabler11@gmail.com>
2024-12-04 11:55:26 -0500

    Add Clothing
    Clothing

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game { gta5 }
+
+
+author 'Austin Cabler'
+description 'FiveM Clothing for Nike T-Shirt'
+version '1.0.0'
+
+files {
+    'mp_m_freemode_01_mp_m_heist3^jbib_017_u.ydd',
+    'mp_m_freemode_01_mp_m_heist3^jbib_diff_017_a_uni.ytd'
+}
